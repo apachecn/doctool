c78d1f11578482c9409dba28823981524a11ec89
飞龙 <admin@flygon.net>
2020-03-12 23:49:51 +0800

    Update diffdither.py

--- diffdither.py
+++ diffdither.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # 四阶灰度扩散仿色
-# python diffdither.py <file> [-m <trunc|grid|strip|noise>]
+# python diffdither.py <file> [-m <trunc|grid|noise>]
 
 import sys
 import cv2
@@ -9,34 +9,43 @@ import numpy as np
 import re
 from argparse import ArgumentParser
 
-def make_noise(size, fc=0, bc=255, k=2):
-    # P(fc) = 1/k, P(bc) = (k-1)/k
+pts = [
+    [0, 0], # 1/16
+    [2, 2],
+    [0, 2],
+    [2, 0],
+    [1, 1],
+    [3, 3],
+    [3, 1],
+    [1, 3],
+    [2, 3],
+    [0, 1],
+    [0, 3],
+    [2, 1],
+    [1, 0],
+    [3, 2],
+    [1, 2],
+    [3, 1], # 16/16
+]
+
+def make_noise(size, fc=0, bc=255, k=8):
+    # P(fc) = k/16, P(bc) = (16-k)/16
     if k == 0: return np.zeros(size) + bc
-    idx = np.random.random(size) < 1/k
+    idx = np.random.random(size) < k/16
     img = np.where(idx, fc, bc)
     return img
 
-def make_strip(size, fc=0, bc=255, k=2):
+def make_grid(size, fc=0, bc=255, k=8):
     img = np.zeros(size) + bc
     for i in range(k):
-        img[i::k, i::k] = fc
-    return img
-
-def make_grid(size, fc=0, bc=255, k=2):
-    if k == 0: return np.zeros(size) + bc
-    if k == 1: return np.zeros(size) + fc
-    img = np.zeros(size) + bc
-    img[0::k, 1::k] = fc
-    img[1::k, 0::k] = fc
-    img[1::k, 2::k] = fc
-    img[2::k, 1::k] = fc
+        r, c = pts[i]
+        img[r::4, c::4] = fc
     return img
     
 
 modes = {
     'grid': make_grid,
     'noise': make_noise,
-    'strip': make_strip,
 }
 
 def greyl4(img, l=4):
@@ -71,16 +80,16 @@ def diffdither(img, mode='grid'):
     settings = [
         {'fc': 0, 'bc': 0, 'k': 0}, # b
         {'fc': 85, 'bc': 0, 'k': 4}, # c11
-        {'fc': 0, 'bc': 85, 'k': 2}, # c12
-        {'fc': 0, 'bc': 85, 'k': 4}, # c13
+        {'fc': 85, 'bc': 0, 'k': 8}, # c12
+        {'fc': 85, 'bc': 0, 'k': 12}, # c13
         {'fc': 0, 'bc': 85, 'k': 0}, # c1
-        {'fc': 170, 'bc': 85, 'k': 4}, # c21
-        {'fc': 85, 'bc': 170, 'k': 2}, # c22
+        {'fc': 85, 'bc': 170, 'k': 12}, # c21
+        {'fc': 85, 'bc': 170, 'k': 8}, # c22
         {'fc': 85, 'bc': 170, 'k': 4}, # c23
         {'fc': 0, 'bc': 170, 'k': 0}, # c2
         {'fc': 255, 'bc': 170, 'k': 4}, # c31
-        {'fc': 170, 'bc': 255, 'k': 2}, # c32
-        {'fc': 170, 'bc': 255, 'k': 4}, # c33
+        {'fc': 255, 'bc': 170, 'k': 8}, # c32
+        {'fc': 255, 'bc': 170, 'k': 12}, # c33
         {'fc': 0, 'bc': 255, 'k': 0}, # w
     ]
 
