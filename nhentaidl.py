095950cc4d578bf967c246b8881a582dbbbacb31
飞龙 <562826179@qq.com>
2020-07-27 10:45:31 +0800

    Add files via upload

---
+++ nhentaidl.py
@@ -0,0 +1,101 @@
+import requests
+from pyquery import PyQuery as pq
+import os
+import sys
+from os import path
+from imgyaso import grid_bts
+import shutil
+import json
+import subprocess as subp
+import uuid
+import tempfile
+
+# npm install -g gen-epub
+
+hdrs = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+}
+
+def safe_mkdir(dir):
+    try:
+        os.mkdir(dir)
+    except:
+        pass
+
+def safe_rmdir(dir):
+    try:
+        shutil.rmtree(dir)
+    except:
+        pass
+        
+def get_info(html):
+    root = pq(html)
+    title = root('h2.title').eq(0).text().strip()
+    imgs = root('.gallerythumb > img')
+    imgs = [
+        pq(i).attr('data-src')
+            .replace('t.jpg', '.jpg')
+            .replace('t.png', '.png')
+            .replace('t.nhentai', 'i.nhentai')
+        for i in imgs
+    ]
+    return {'title': title, 'imgs': imgs}
+
+def gen_epub(articles, imgs=None, name=None, out_path=None):   
+    imgs = imgs or {}
+
+    dir = path.join(tempfile.gettempdir(), uuid.uuid4().hex) 
+    safe_mkdir(dir)
+    img_dir = path.join(dir, 'img')
+    safe_mkdir(img_dir)
+    
+    for fname, img in imgs.items():
+        fname = path.join(img_dir, fname)
+        with open(fname, 'wb') as f:
+            f.write(img)
+    
+    fname = path.join(dir, 'articles.json')
+    with open(fname, 'w') as f:
+        f.write(json.dumps(articles))
+    
+    args = [
+        'gen-epub',
+        fname,
+        '-i',
+        img_dir,
+    ]
+    if name: args += ['-n', name]
+    if out_path: args += ['-p', out_path]
+    subp.Popen(
+        args, shell=True, 
+        stdout=subp.PIPE, 
+        stderr=subp.PIPE
+    ).communicate()
+    safe_rmdir(dir)
+
+def main():
+    
+    id = sys.argv[1]
+    url = f'https://nhentai.net/g/{id}/'
+    html = requests.get(url).text
+    info = get_info(html)
+    print(info['title'])
+    
+    imgs = {}
+    l = len(str(len(info['imgs'])))
+    for i, img_url in enumerate(info['imgs']):
+        fname = str(i).zfill(l) + '.png'
+        print(f'{img_url} => {fname}')
+        img = requests.get(img_url, headers=hdrs).content
+        img = grid_bts(img)
+        imgs[fname] = img
+            
+    co = [
+        f'<p><img src="../Images/{str(i).zfill(l)}.png" /></p>' 
+        for i in range(len(info['imgs']))
+    ]
+    co = '\n'.join(co)
+    articles = [{'title': info['title'], 'content': co}]
+    gen_epub(articles, imgs)
+
+if __name__ == '__main__': main()
