f20dc8ce4523801343c840cab2e60fa615708b70
布客飞龙 <admin@flygon.net>
2021-08-28 14:07:40 +0800

    Add files via upload

---
+++ gzhlist.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from urllib.parse import quote_plus
+import os
+import sys
+
+token = os.environ.get('WX_TOKEN', '')
+cookie = os.environ.get('WX_COOKIE', '')
+
+headers = {'Cookie': cookie}
+
+def main():
+    name = sys.argv[1]
+    url = f'https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=1&query={quote_plus(name)}&token={token}&lang=zh_CN&f=json&ajax=1'
+    j = requests.get(url, headers=headers).json()
+    fake_id = j['list'][0]['fakeid']
+    
+    ofile = open(f'wx_{name}.txt', 'w')
+    i = 0
+    while True:
+        print(f'page: {i // 5 + 1}')
+        url = f'https://mp.weixin.qq.com/cgi-bin/appmsg?action=list_ex&begin={i}&count=5&fakeid={fake_id}&type=9&query=&token={token}&lang=zh_CN&f=json&ajax=1'
+        j = requests.get(url, headers=headers).json()
+        if len(j['app_msg_list']) == 0:
+            break
+        for it in j['app_msg_list']:
+            print(it['link'])
+            ofile.write(it['link'] + '\n')
+        i += 5
+    ofile.close()
+    print('done...')
+    
+if __name__ == '__main__': main()
