e60c7eec355add438b1236ba9807443f98eef828
布客飞龙 <admin@flygon.net>
2021-08-31 18:12:38 +0800

    Update gzhlist.py

--- gzhlist.py
+++ gzhlist.py
@@ -4,8 +4,8 @@ from urllib.parse import quote_plus
 import os
 import sys
 import time
+import re
 
-token = os.environ.get('WX_TOKEN', '')
 cookie = os.environ.get('WX_COOKIE', '')
 
 headers = {'Cookie': cookie}
@@ -13,6 +13,15 @@ headers = {'Cookie': cookie}
 def main():
     name = sys.argv[1]
     start = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    url = f'https://mp.weixin.qq.com'
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    loc = r.headers.get('Location', '')
+    m = re.search(r'token=(\d+)', loc)
+    if not m:
+        print('token 获取失败')
+        return
+    token = m.group(1)
+    
     url = f'https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=1&query={quote_plus(name)}&token={token}&lang=zh_CN&f=json&ajax=1'
     j = requests.get(url, headers=headers).json()
     fake_id = j['list'][0]['fakeid']
@@ -24,7 +33,6 @@ def main():
         print(f'page: {i // 5 + 1}')
         url = f'https://mp.weixin.qq.com/cgi-bin/appmsg?action=list_ex&begin={i}&count=5&fakeid={fake_id}&type=9&query=&token={token}&lang=zh_CN&f=json&ajax=1'
         j = requests.get(url, headers=headers).json()
-        print(j)
         if j['base_resp']['ret'] == 200013:
             time.sleep(wait)
             wait += 10
