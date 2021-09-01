ffbea4f2ca9b807f10bb7cd5703b28cfd19a2254
布客飞龙 <admin@flygon.net>
2021-09-01 14:19:08 +0800

    Update and rename gzhlist.py to gzh.py

--- gzh.py
+++ gzh.py
@@ -1,10 +1,12 @@
 import requests
 import json
-from urllib.parse import quote_plus
 import os
 import sys
 import time
 import re
+from datetime import datetime
+import subprocess as subp
+import uuid
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
@@ -20,7 +22,8 @@ def parse_kv(text):
 
 def main():
     param = parse_kv(sys.argv[1])
-    start = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    start = sys.argv[2] if len(sys.argv) > 2 else None
+    end = sys.argv[3] if len(sys.argv) > 3 else None
     biz = param.get('__biz', '')
     uin = param.get('uin', '')
     key = param.get('key', '')
@@ -36,10 +39,11 @@ def main():
     name = m.group(1)
     print(name)
     
-    ofile = open(f'wx_{name}.txt', 'a')
+    urls = []
     wait = 10
-    i = (start - 1) * 10
-    while True:
+    i = 0
+    stop = False
+    while not stop:
         print(f'page: {i // 10 + 1}')
         url = f'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz={biz}&f=json&offset={i}&count=10&is_ok=1&scene=&uin={uin}&key={key}&pass_ticket={pass_ticket}&wxtoken=&appmsg_token={appmsg_token}&x5=0&f=json'
         j = requests.get(url, headers=headers).json()
@@ -52,12 +56,33 @@ def main():
             break
         li = json.loads(j['general_msg_list'])
         for it in li['list']:
+            dt = datetime \
+                .utcfromtimestamp(it['comm_msg_info']['datetime']) \
+                .strftime('%Y%m%d')
+            if start and dt < start:
+                stop = True
+                break
+            if end and dt > end:
+                continue
             url = it['app_msg_ext_info']['content_url']
+            if not url: continue
             print(url)
-            ofile.write(url + '\n')
+            urls.append(url)
         wait = 10
         i += 10
-    ofile.close()
+    
+    config = {
+        "name": f"{name} {start}-{end}",
+        "url": "https://mp.weixin.qq.com",
+        "title": "#activity-name",
+        "content": "#js_content",
+        "optiMode": "thres",
+        "list": urls,
+    }
+    config_fname = f'config_{uuid.uuid4().hex}.json'
+    open(config_fname, 'w', encoding='utf-8').write(json.dumps(config))
+    subp.Popen(f'crawl-epub {config_fname}', shell=True).communicate()
+    os.remove(config_fname)
     print('done...')
     
 if __name__ == '__main__': main()
