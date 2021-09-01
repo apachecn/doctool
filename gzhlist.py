6c37bb99e83e7c7d67309b95a63e80800f27e79e
布客飞龙 <admin@flygon.net>
2021-09-01 20:25:47 +0800

    Add files via upload

---
+++ gzhlist.py
@@ -0,0 +1,50 @@
+import requests
+import json
+from urllib.parse import quote_plus
+import os
+import sys
+import time
+import re
+
+cookie = os.environ.get('WX_COOKIE', '')
+
+headers = {'Cookie': cookie}
+
+def main():
+    name = sys.argv[1]
+    start = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    url = f'https://mp.weixin.qq.com'
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    loc = r.headers.get('Location', '')
+    m = re.search(r'token=(\d+)', loc)
+    if not m:
+        print('token 获取失败')
+        return
+    token = m.group(1)
+    
+    url = f'https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=1&query={quote_plus(name)}&token={token}&lang=zh_CN&f=json&ajax=1'
+    j = requests.get(url, headers=headers).json()
+    fake_id = j['list'][0]['fakeid']
+    
+    ofile = open(f'wx_{name}.txt', 'a')
+    wait = 10
+    i = (start - 1) * 5
+    while True:
+        print(f'page: {i // 5 + 1}')
+        url = f'https://mp.weixin.qq.com/cgi-bin/appmsg?action=list_ex&begin={i}&count=5&fakeid={fake_id}&type=9&query=&token={token}&lang=zh_CN&f=json&ajax=1'
+        j = requests.get(url, headers=headers).json()
+        if j['base_resp']['ret'] == 200013:
+            time.sleep(wait)
+            wait += 10
+            continue
+        if len(j['app_msg_list']) == 0:
+            break
+        for it in j['app_msg_list']:
+            print(it['link'])
+            ofile.write(it['link'] + '\n')
+        wait = 10
+        i += 5
+    ofile.close()
+    print('done...')
+    
+if __name__ == '__main__': main()
