dab8039fbd6b0fba910b64a82782d8c3e3e5957c
布客飞龙 <admin@flygon.net>
2022-01-03 14:26:07 +0800

    Add files via upload

---
+++ fetch_pages.py
@@ -0,0 +1,46 @@
+import sys
+from urllib.parse import urljoin
+from EpubCrawler.util import request_retry
+from pyquery import PyQuery as pq
+
+config = {
+    'url': 'https://www.geeksforgeeks.org/category/html/page/{i}/',
+    'link': '.articles-list .content .head a',
+    'proxy': None,
+}
+
+def get_toc(html, base):
+    root = pq(html)
+    el_links = root(config['link'])
+    links = []
+    for i in range(len(el_links)):
+        url = el_links.eq(i).attr('href')
+        if not url:
+            links.append(el_links.eq(i).text().strip())
+            continue
+        links.append(urljoin(base, url))
+    return links
+
+def main():
+    ofname = sys.argv[1]
+    st = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+    ed = int(sys.argv[3]) if len(sys.argv) > 3 else 100_000_000
+    
+    ofile = open(ofname, 'a', encoding='utf-8')
+    
+    for i in range(st, ed + 1):
+        print(f'page: {i}')
+        url = config['url'].replace('{i}', str(i))
+        html = request_retry(
+            'GET', url, 
+            proxies=config['proxy'],
+        ).text
+        toc = get_toc(html, url)
+        if len(toc) == 0: break
+        for it in toc:
+            print(it)
+            ofile.write(it + '\n')
+    
+    ofile.close()
+    
+if __name__ == '__main__': main()
